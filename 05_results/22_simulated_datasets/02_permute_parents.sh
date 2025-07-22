3d5d4acbacafe69d3a86132a1569bac8976ffb2b
Thomas Ellis <thomas.ellis@gmi.oeaw.ac.at>
2025-07-22 14:06:47 +0200

    Fix permutations of parents genotypes
    Commit of file to permute parental genotypes that should have been in a
    previous commit.
    
     Changes to be committed:
    	modified:   05_results/22_simulated_datasets/02_permute_parents.sh

--- 05_results/22_simulated_datasets/02_permute_parents.sh
+++ 05_results/22_simulated_datasets/02_permute_parents.sh
@@ -1,17 +1,22 @@
+#!/usr/bin/env bash
+
 # Permute genotypes at each locus for the parents.
 #
 # This applies the python script 01_permute_VCF.py to the VCF file for the 
-# parents to randomise genotypes at each marker. This is done 200 times.
+# parents to randomise genotypes at each marker.
+# The resulting files are then converted to Plink format
+#
+# This is done 200 times as an array job.
 #
 # Tom Ellis, 21st July 2025
 
 # SLURM
-#SBATCH --job-name=02_permute_VCF
-#SBATCH --output=03_processing/08_simulated_datasets/slurm/%x-%a.out
-#SBATCH --error=03_processing/08_simulated_datasets/slurm/%x-%a.err
-#SBATCH --mem=1GB
-#SBATCH --qos=short
-#SBATCH --time=4:00:00
+#SBATCH --job-name=02_permute_parents
+#SBATCH --output=05_results/22_simulated_datasets/slurm/%x-%a.out
+#SBATCH --error=05_results/22_simulated_datasets/slurm/%x-%a.err
+#SBATCH --mem=5GB
+#SBATCH --qos=rapid
+#SBATCH --time=1:00:00
 #SBATCH --array=1-200
 
 # Set working directory and load the conda environment
@@ -28,19 +33,29 @@ in_vcf='03_processing/05_imputation/output/parental_lines.vcf.gz'
 
 # === Output ===
 
-outdir=$scratchdir/08_simulated_datasets/${SLURM_JOB_NAME}
+outdir=$scratchdir/22_simulated_datasets/${SLURM_JOB_NAME}
 mkdir -p $outdir
+
+# Prefix for output files without any file extension
+output_prefix=$outdir/permutd_$(printf "%03d\n" $i)
+
 # Output VCF file with genotypes at each locus in a random order.
-out_vcf=$outdir/permutd_$(printf "%03d\n" $i)
+out_vcf=${output_prefix}.vcf
 
 
 # === Main ===
 
 # Permute genotypes
-python 03_processing/08_simulated_datasets/01_permute_VCF.py \
+echo "Permuting genotypes for replicate ${i}."
+python 05_results/22_simulated_datasets/01_permute_VCF.py \
     --input $in_vcf \
     --output $out_vcf \
     --seed $i
-    
-# Index the output file
-tabix $out_vcf
+
+# Create Plink files
+echo "Converting to Plink format."
+plink2 \
+  --vcf $out_vcf \
+  --make-bed \
+  --set-all-var-ids @_# \
+  --out $output_prefix
