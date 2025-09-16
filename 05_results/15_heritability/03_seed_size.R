51ff095a6f211085334020620950f1db07dabb49
Thomas Ellis <thomas.ellis@gmi.oeaw.ac.at>
2025-09-16 17:05:13 +0200

    Added broad-sense heritability for seed size.
     On branch main
     Changes to be committed:
    	modified:   05_results/15_heritability/02_rosette_size.R
    	new file:   05_results/15_heritability/03_seed_size.R
    	modified:   05_results/15_heritability/result_summary.md

--- 05_results/15_heritability/03_seed_size.R
+++ 05_results/15_heritability/03_seed_size.R
@@ -1,14 +1,20 @@
-#' Calculate broad-sense heritability for flowering time
+#' Calculate broad-sense heritability for seed size
 #'
-#' This collates raw flowering time data, estimates variance explained by
+#' This collates raw seed-size data, estimates variance explained by
 #' genetics and experimental design, then expresses these estimates as a proportion
-#' of total phenotypic variation on the scale of the data data.
+#' of total phenotypic variation on the scale of the data.
+#' This is done separately for parents, and both cohorts of F9s.
+#'
+#' Tom Ellis, 16th September 2025
 
 
 library('lme4')
+library('QGglmm')
 
-# Collate and format raw flowering-time data
-source("03_processing/06_process_phenotypes/02_rosette_size.R")
+# Run formatting code used to create BLUPs.
+# This saves a lot of boilerplate, but takes a long time because there is a
+# heavy GLM to run, which is not actually not needed here.
+source("03_processing/06_process_phenotypes/05_seed_size.R")
 
 pops <- c("parent", "rep1", "rep2")
 
@@ -16,17 +22,22 @@ pops <- c("parent", "rep1", "rep2")
 list_pve <- vector('list', 3)
 names(list_pve) <- pops
 
-# Variance components for three cohorts
+
 for (group in pops){
 
   # Model flowering time as a function of genotype, cohort and tray
-  rosette_model <- lmer(
-    sqrt(rosette) ~ (1 | tray) + (1 | genotype) + (1|id),
-    data = raw_rosette_size, subset = cohort==group
+  mod_ss <- lmer(
+    surface_area ~ (1 | rep/tray) + (1| genotype/label),
+    data=raw_seed_sizes,
+    subset = cohort==group
   )
+  # Extract variance components on the expected scale
+  vc <- as_tibble(VarCorr(mod_ss))
+  vc$pve <- vc$vcov / sum(vc$vcov)
+
   # Extract variance components on the data scale
   # The model uses and identity link, so data and expected scales are identical
-  vc <- as.data.frame(lme4::VarCorr(rosette_model))
+  vc <- as.data.frame(lme4::VarCorr(mod_ss))
   pve_data_scale <- vc$vcov / sum(vc$vcov)
 
   list_pve[[group]] <- pve_data_scale
@@ -35,14 +46,14 @@ for (group in pops){
 # Data from with a row for the three groups, and columns for var explained by
 # each component
 h2 <- do.call('rbind', list_pve)
-colnames(h2) <- c('Measurement', 'Genotype', 'Tray', 'residuals')
+colnames(h2) <- c('Mother', 'Genotype', 'Tray', 'Replicate', 'Residuals')
 h2 <- as_tibble(h2, rownames = "group")
 
 # Plot a stacked bar chart with group on the x and var explained on the y
 # Results are nearly identical for the three groups
 # Genetics explains about 80% of the variance.
 h2 %>%
-  pivot_longer(Measurement:Tray, names_to = "component", values_to = "pve") %>%
+  pivot_longer(Mother:Residuals, names_to = "component", values_to = "pve") %>%
   ggplot(aes(x = group, y = pve, fill=component)) +
   geom_col()+
   lims(
--- 05_results/15_heritability/result_summary.md
+++ 05_results/15_heritability/result_summary.md
@@ -14,8 +14,12 @@ Scripts estimate broad-sense heritability as a random effect of var between geno
 
 ## Main conclusion
 
-Flowering time is really heritable.
-Rosette size much less so.
+- For all traits, variance components are pretty consistent between cohorts.
+- Flowering time is really heritable.
+- Rosette size H2~0.4 for parents and F9 cohort 1, but only 0.23 for cohort 2.
+  - Differences between measurements are a similar magnitude.
+- Seed size is H2~0.3.
+  - Big differences between mothers.
 
 ## Caveats
 
