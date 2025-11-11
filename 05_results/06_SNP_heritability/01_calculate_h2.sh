f97ca8b8bec9b742e9db7dce8e7dcfc2cddf03da
Thomas Ellis <thomas.ellis@gmi.oeaw.ac.at>
2025-11-11 13:27:55 +0100

    Tidy up of script to calculate SNP heritability.
     Changes to be committed:
       (use "git reset HEAD <file>..." to unstage)
    	modified:   05_results/06_SNP_heritability/01_calculate_h2.sh

--- 05_results/06_SNP_heritability/01_calculate_h2.sh
+++ 05_results/06_SNP_heritability/01_calculate_h2.sh
@@ -2,64 +2,64 @@
 
 # Calculate SNP heritabilities using GEMMA.
 #
-# Tom Ellis, 28th June 2024
+# Tom Ellis, 16th September 2025
 
 # SLURM
-#SBATCH --job-name=snp_h2
-#SBATCH --output=slurm/%x-%a.out
-#SBATCH --error=slurm/%x-%a.err
+#SBATCH --job-name=01_calculate_h2
+#SBATCH --output=05_results/06_SNP_heritability/slurm/%x-%a.out
+#SBATCH --error=05_results/06_SNP_heritability/slurm/%x-%a.err
 #SBATCH --mem=10GB
-#SBATCH --qos=short
-#SBATCH --time=4:00:00
-#SBATCH --array=2
-
-date 
+#SBATCH --qos=rapid
+#SBATCH --time=1:00:00
+#SBATCH --array=0-11
 
 # Load conda environment
 # If you haven't already, install the environment with `conda env create -f environment.yml`
 source setup.sh
 
-i=$SLURM_ARRAY_TASK_ID
+
 
 # ===Input files ==== #
 
+i=$SLURM_ARRAY_TASK_ID
+
+# Array of text files with Blup phenotypes.
+blup_array=(03_processing/06_process_phenotypes/output/*blups*tsv)
+pheno_file=${blup_array[$i]}
+
 # Array of VCF files for the two F8 replicates and the parents
-vcf_dir=03_processing/04_pieters_VCF/output
-vcf_files=($vcf_dir/F8_snp_matrix_purged_rep1.vcf.gz $vcf_dir/F8_snp_matrix_purged_rep2.vcf.gz 03_processing/04_pieters_VCF/output/parental_snp_matrix.vcf.gz)
-# File to use in this job
-input_vcf=${vcf_files[$i]}
+parents_vcf=03_processing/09_impute_haplotypes/output/parental_lines.vcf.gz
+progeny_vcf=03_processing/09_impute_haplotypes/output/F8_imputed.vcf.gz
 
-# Array of files containing genetic values for rosette size
-blup_dir=03_processing/06_process_phenotypes/output
-rs_files=($blup_dir/rosette_size_blups_F9_rep1.tsv $blup_dir/rosette_size_blups_F9_rep2.tsv $blup_dir/rosette_size_blups_parents.tsv)
-input_rs=${rs_files[$i]}
-
-# Array of files containing genetic values for seed size
-ss_files=($blup_dir/seed_size_blups_F9_rep1.tsv $blup_dir/seed_size_blups_F9_rep2.tsv $blup_dir/seed_size_blups_parents.tsv)
-input_ss=${ss_files[$i]}
 
 # === Output files ===
 
 # Output directory
-outdir=05_results/06_SNP_heritability/output
+outdir=$scratchdir/06_SNP_heritability
 mkdir -p $outdir
+
+# Basename for the file containing the PVE estimate.
+pve_file=$(basename -s .tsv $pheno_file)_pve.txt
 
 # === Script ===
 
-echo "\nVCF file: ${input_vcf}"
-echo "Rosette size phenotype file: ${input_rs}"
-echo "Output directory: ${outdir}\n\n"
+if [[ "$pheno_file" == *"parent"* ]]; then
+    echo "Phenotype filename contains 'parent'"
+    input_vcf=$parents_vcf
+else
+    echo "Phenotype filename does not contain 'parent'"
+    input_vcf=$progeny_vcf
+fi
 
-echo "Calculating SNP heritability for rosette size"
+echo "VCF file: ${input_vcf}"
+echo "Phenotype file: ${pheno_file}"
+echo "Output directory: ${outdir}"
+echo ""
+
+echo "Calculating SNP heritability."
 02_library/run_SNP_heritability.sh \
   --vcf $input_vcf \
-  --phenotypes $input_rs \
+  --phenotypes $pheno_file \
   --outdir $outdir
 
-echo "SNP heritability for seed size"
-02_library/run_SNP_heritability.sh \
-  --vcf $input_vcf \
-  --phenotypes $input_ss \
-  --outdir $outdir
-
-date
+cp $outdir/$pve_file 05_results/06_SNP_heritability/output/
